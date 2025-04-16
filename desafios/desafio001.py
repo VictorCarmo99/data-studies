128eac9b50c33dad26afd262172e7a7a0f72c80d
VictorCarmo99 <vict.99carmo@gmail.com>
2025-04-16 14:18:40 -0300

    Create desafio001.py
    Desafio Proposto em:
    Curso Python #04 - Primeiros comandos em Python3

---
+++ desafios/desafio001.py
@@ -0,0 +1,6 @@
+#Perguntar ao usuário qual o nome dele#
+nome = input('Qual é o seu nome?: ')
+
+#Retornar a mensagem de boas vindas#
+print('Seja Bem vindo,', nome, '! Prazer em te conhecer')
+      
