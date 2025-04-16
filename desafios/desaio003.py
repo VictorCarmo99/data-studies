665aef2d7d97315f90cb009f3308fdae5369d542
VictorCarmo99 <vict.99carmo@gmail.com>
2025-04-16 14:28:05 -0300

    Create desaio003.py

---
+++ desafios/desaio003.py
@@ -0,0 +1,10 @@
+#Mensagem de Apresentação#
+print('Olá, serei sua calculadora de Adição em Python, informe Números Inteiros à seguir.\n')
+
+#Váriaveis a calcular#
+prim_num = int(input('Primeiro Número: '))
+seg_num = int(input('Segundo Número: '))
+
+#Retornar o valor da Soma para o Usuário#
+print(prim_num + seg_num)
+      
