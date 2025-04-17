9eb0c86d51b10f2fe13a379e99db66b6b22feffb
VictorCarmo99 <vict.99carmo@gmail.com>
2025-04-17 14:02:44 -0300

    Update desafio003.py
    Melhoria do código após a aula #06 do curso em que aprendemos a usar o .format()

--- desafios/desafio003.py
+++ desafios/desafio003.py
@@ -4,7 +4,7 @@ print('Olá, serei sua calculadora de Adição em Python, informe Números Intei
 #Váriaveis a calcular#
 prim_num = int(input('Primeiro Número: '))
 seg_num = int(input('Segundo Número: '))
+som = prim_num + seg_num
 
 #Retornar o valor da Soma para o Usuário#
-print('A soma Vale: ', prim_num + seg_num)
-      
+print('A Soma entre {} e {} vale: {}' .format(prim_num, seg_num, som))
