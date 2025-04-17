b70e819a9db7131696c7d4758281d28ca7b8a435
VictorCarmo99 <vict.99carmo@gmail.com>
2025-04-17 12:58:07 -0300

    Create ex002.py

---
+++ PythonExercicios/ex002.py
@@ -0,0 +1,8 @@
+#Desafio 002#
+#Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas#
+
+#Solicitação da váriavel para o Usuário#
+nome = input("Olá, qual é o seu nome?\nResposta: ")
+
+#Mensagem de Boas-Vindas#
+print("Seja Bem vindo, {}!".format(nome))
