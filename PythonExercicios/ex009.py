59dc00ad7aec18cb7270d8fdc7c5fc8d0d7f0934
VictorCarmo99 <vict.99carmo@gmail.com>
2025-05-13 17:13:02 -0300

    Create ex009.py

---
+++ PythonExercicios/ex009.py
@@ -0,0 +1,21 @@
+# Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+print('Gerador de Tabuadas Automático.\n')
+
+n = int(input('Informe um número para gerar a tabuada: '))
+
+# Tabuada
+print(f'\n','='*12,'\n'
+      f' Tabuada do {n}\n',
+      f'='*12,'\n'
+      f'{n:2} X  1 = {n * 1:2}\n'
+      f'{n:2} X  2 = {n * 2:2}\n'
+      f'{n:2} X  3 = {n * 3:2}\n'
+      f'{n:2} X  4 = {n * 4:2}\n'
+      f'{n:2} X  5 = {n * 5:2}\n'
+      f'{n:2} X  6 = {n * 6:2}\n'
+      f'{n:2} X  7 = {n * 7:2}\n'
+      f'{n:2} X  8 = {n * 8:2}\n'
+      f'{n:2} X  9 = {n * 9:2}\n'
+      f'{n:2} X 10 = {n * 10:2}\n',
+      f'='*12)
