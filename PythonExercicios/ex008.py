c2eeb4db47807b04ec2ebd55225fcad9793fdc9b
VictorCarmo99 <vict.99carmo@gmail.com>
2025-05-13 16:46:20 -0300

    Create ex008.py

---
+++ PythonExercicios/ex008.py
@@ -0,0 +1,11 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+print('Conversor de Unidades de Medida de Comprimento\n')
+
+metros = float(input('Digite o valor a converter EM METROS: '))
+cent = metros * 100
+mili = metros * 1000
+
+print(f'{metros}m convertido equivale à:\n'
+      f'Centrimetros: {cent:.0f}\n'
+      f'Milimetros: {mili:.0f}')
