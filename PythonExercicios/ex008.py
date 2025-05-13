cf057088d3e85e54b9a61854e20cb27930af6e1f
VictorCarmo99 <vict.99carmo@gmail.com>
2025-05-13 15:38:59 -0300

    Create ex008.py

---
+++ PythonExercicios/ex008.py
@@ -0,0 +1,14 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+print('Olá! Vamos Calcular a Média das suas notas da escola?\n')
+
+materia = input('Disciplina: ')
+
+t1 = float(input('Primeiro Trimestre: '))
+t2 = float(input('Segundo Trimestre: '))
+t3 = float(input('Terceiro Trimestre: '))
+t4 = float(input('Quarto Trimestre: '))
+
+media = (t1 + t2 + t3 + t4) / 4
+
+print(f'A média das suas notas esse ano em {materia} foi de: {media}')
