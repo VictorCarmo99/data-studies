3f0074884edfe3477f6c315a21692c7480c424b5
VictorCarmo99 <vict.99carmo@gmail.com>
2025-05-21 13:07:06 -0300

    Update ex002.py

--- python/curso-em-video/mundo1/ex002.py
+++ python/curso-em-video/mundo1/ex002.py
@@ -1,8 +1,8 @@
-# Exercicio Python 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas#
+# Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 
-#Solicitação da váriavel para o Usuário#
+#Solicitação da váriavel para o Usuário
 nome = input("Olá, qual é o seu nome?\nResposta: ")
 
-#Mensagem de Boas-Vindas#
+#Mensagem de Boas-Vindas
 print("Seja Bem vindo, {}!".format(nome))
 
