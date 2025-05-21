6cd7bb20054deb5d02f2f86d14d3f0fb251b0b15
VictorCarmo99 <vict.99carmo@gmail.com>
2025-05-21 13:04:58 -0300

    Update and rename python/curso-em-video/ex003.py to python/curso-em-video/mundo1/ex003.py

--- python/curso-em-video/mundo1/ex003.py
+++ python/curso-em-video/mundo1/ex003.py
@@ -1,3 +1,5 @@
+# Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
 soma = n1 + n2
